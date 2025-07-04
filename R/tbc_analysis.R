03a73643211f162925362262f527b3b729a9fec9
Taobo Hu <taobo.hu@scilifelab.se>
2025-07-04 12:28:44 +0200

    Update tbc_analysis.R

--- R/tbc_analysis.R
+++ R/tbc_analysis.R
@@ -117,11 +117,11 @@ transcript_by_cell_analysis <- function(cell_metadata,
                   n_jobs, length(genes)))
 
   handlers("progress")  # CLI 进度条
-  plan(future::multicore, workers = n_jobs)
-  on.exit(plan(sequential), add = TRUE)  # 运行结束后恢复单线程
+  future::plan("multicore", workers = n_jobs)
+  on.exit(future::plan("sequential"), add = TRUE)  # 运行结束后恢复单线程
 
   results_list <- with_progress({
-    p <- progressor(steps = length(genes))
+    p <- progressr::progressor(steps = length(genes))
 
     future_lapply(
       X   = genes,
